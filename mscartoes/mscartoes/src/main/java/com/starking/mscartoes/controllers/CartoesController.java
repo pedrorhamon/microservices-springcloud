f856473d13779ea9653a3a17bec5d8c33b1c28e6
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-20 11:49:25 -0300

    criando ms cartoes e integrando com eureka

---
+++ mscartoes/mscartoes/src/main/java/com/starking/mscartoes/controllers/CartoesController.java
@@ -0,0 +1,15 @@
+package com.starking.mscartoes.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cartoes")
+public class CartoesController {
+	
+	@GetMapping
+	public String status() {
+		return "cartoes";
+	}
+}
