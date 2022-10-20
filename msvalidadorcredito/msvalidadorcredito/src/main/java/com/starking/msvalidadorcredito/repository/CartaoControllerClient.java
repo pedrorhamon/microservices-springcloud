36cba0accdeed94b054de67df47307c3db275b1b
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-20 16:24:24 -0300

    Buscando os dados de cartoes pelo microservice de cartoes

---
+++ msvalidadorcredito/msvalidadorcredito/src/main/java/com/starking/msvalidadorcredito/repository/CartaoControllerClient.java
@@ -0,0 +1,17 @@
+package com.starking.msvalidadorcredito.repository;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.starking.msvalidadorcredito.domain.CartaoCliente;
+
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartaoControllerClient {
+	
+	@GetMapping(params = "cpf")
+	ResponseEntity<List<CartaoCliente>> getCartoesByCliente(@RequestParam("cpf") String cpf);
+}
