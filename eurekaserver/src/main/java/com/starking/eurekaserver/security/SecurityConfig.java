81e1dd96a895b57be3ea7e92f209ce7f57b82fc6
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-22 20:07:58 -0300

    configurando url para seguranca nas api

---
+++ eurekaserver/src/main/java/com/starking/eurekaserver/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.starking.eurekaserver.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig  {
+
+//	@Bean
+//	public void configure(HttpSecurity http) throws Exception {
+//		http
+//		.csrf()
+//		.disable()
+//		.authorizeRequests()
+//		.anyRequest().authenticated()
+//		.and()
+//		.httpBasic();
+//	}
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+                .anyRequest().authenticated()
+            )
+            .httpBasic();
+        return http.build();
+    }
+}
--- mscartoes/mscartoes/src/main/java/com/starking/mscartoes/MscartoesApplication.java
+++ mscartoes/mscartoes/src/main/java/com/starking/mscartoes/MscartoesApplication.java
@@ -5,12 +5,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableRabbit
+@Slf4j
 public class MscartoesApplication {
 
 	public static void main(String[] args) {
+		log.info("Informação: {}", "teste info");
+		log.error("Error: {}", "teste error");
+		log.warn("Aviso: {}", "teste warn");
 		SpringApplication.run(MscartoesApplication.class, args);
 	}
 
